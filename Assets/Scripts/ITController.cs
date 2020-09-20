97ddb8f5f6bd8c474d24e99fd8f4dceea55b639e
CYFERIOUS <frankdrujes@gmail.com>
2020-09-20 21:21:32 +0200

    score rotate skull done

--- Assets/Scripts/ITController.cs
+++ Assets/Scripts/ITController.cs
@@ -12,12 +12,16 @@ public class ITController : MonoBehaviour,  ITrackableEventHandler
     public GameObject findText;
     private bool started;
     public TMP_Text texto;
-
-
+    Animator _evil;
+    private int score = 0;
+    private int evilScore = 0;
     void Start()
     {
         //texto = GameObject.Find("Canvaso").GetComponentInChildren<TMP_Text>();
-    
+       _evil = GameObject.Find("oracle").GetComponentInChildren<Animator>();
+        
+        score = 46;
+        evilScore = score * 9;
         texto = this.gameObject.GetComponentInChildren<TMP_Text>();
 
         mTrackableBehaviour = GetComponent<TrackableBehaviour>();
@@ -28,21 +32,70 @@ public class ITController : MonoBehaviour,  ITrackableEventHandler
         started = false;
     }
 
+
     public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
     {
         if (newStatus == TrackableBehaviour.Status.DETECTED ||
             newStatus == TrackableBehaviour.Status.TRACKED ||
             newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
         {
-            Debug.Log(mTrackableBehaviour.TrackableName);
-            texto.text = mTrackableBehaviour.name;
+            //Debug.Log(mTrackableBehaviour.TrackableName);
+            //texto.text = mTrackableBehaviour.name;
             // Play audio when target is found
+          
             if (!started)
             {
+
+                //audio.Play();
+                //findText.gameObject.SetActive(false);
                
-                audio.Play();
-                findText.gameObject.SetActive(false);
                 started = true;
+                if (evilScore > 0 && evilScore <= 74)
+                {
+                    _evil.SetInteger("goTo", 1);
+                    texto.text = 1.ToString();
+                }
+                if (evilScore > 74 && evilScore <= 148)
+                {
+                    _evil.SetInteger("goTo", 2);
+                    texto.text = 2.ToString();
+                }
+                if (evilScore > 148 && evilScore <= 222)
+                {
+                    _evil.SetInteger("goTo", 3);
+                    texto.text = 3.ToString();
+                }
+                if (evilScore > 222 && evilScore <= 296)
+                {
+                    _evil.SetInteger("goTo", 4);
+                    texto.text = 4.ToString();
+                }
+                if (evilScore > 296 && evilScore <= 370)
+                {
+                    _evil.SetInteger("goTo", 5);
+                    texto.text = 5.ToString();
+                }
+                if (evilScore > 370 && evilScore <= 444)
+                {
+                    _evil.SetInteger("goTo", 6);
+                    texto.text = 6.ToString();
+                }
+                if (evilScore > 444 && evilScore <= 518)
+                {
+                    _evil.SetInteger("goTo", 7);
+                    texto.text = 7.ToString();
+                }
+                if (evilScore > 518 && evilScore <= 592)
+                {
+                    _evil.SetInteger("goTo", 8);
+                    texto.text = 8.ToString();
+                }
+                if (evilScore > 592)
+                {
+                    _evil.SetInteger("goTo", 9);
+                    texto.text = 9.ToString();
+                }
+
             }
         }
         else
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ORAQARO
 AR CURSED GAME
+please use the images attached in the folder imagoDemo for visualize the spectral forces of evil.
