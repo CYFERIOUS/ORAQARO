f70a6df3550c0ae0cef18ab3b9a86302f3f2b4f1
CYFERIOUS <frankdrujes@gmail.com>
2021-10-20 16:16:22 -0500

    monolith over letter is done

--- Assets/Scripts/clickedEst.cs
+++ Assets/Scripts/clickedEst.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class clickedEst : MonoBehaviour
 {
@@ -9,42 +12,42 @@ public class clickedEst : MonoBehaviour
     private string[] dataBoard = new string[2];
 
     public Canvas canvas;
-    //public Canvas canvas2;
+    public Text myText;
+    Button[] buttons;
+
+
 
     void Start()
     {
         canvas = (Canvas)GameObject.FindObjectOfType(typeof(Canvas));
+        buttons = canvas.GetComponentsInChildren<Button>();
+
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            int closureIndex = i; // Prevents the closure problem
+            buttons[closureIndex].onClick.AddListener(() => TaskOnClick(closureIndex));
+        }
+
 
     }
 
-    public void OnButtonClick()
+    private void TaskOnClick(int buttonIndex)
     {
-     
-        var go = EventSystem.current.currentSelectedGameObject;
-        if (go != null)
-        {
-            //Debug.Log("Clicked on : " + go.name);
-            if (canvas.name == "Zona")
-            {
-                zoneBoard(go.name);
-            }
-            if (canvas.name == "Ouija")
-            {
-               
-                letterBoard(go.name);
-            }
-
-        }
-
-        else
-        {
-            Debug.Log("currentSelectedGameObject is null");
-        }
-           
+      
+        Debug.Log("You have clicked the button #" + buttons[buttonIndex].name, buttons[buttonIndex]);
+        
+        GameObject ficha = buttons[buttonIndex].transform.GetChild(0).gameObject;
+       
+            myText.text = ficha.name;
+        ficha.SetActive(true);
+      
     }
+
+    
     public void zoneBoard(string zone)
     {
         dataBoard[0] = zone;
+        myText.text = "PIPOPEPO";
         Debug.Log("zona : " + dataBoard[0].ToString());
     }
     public void letterBoard(string letter)
