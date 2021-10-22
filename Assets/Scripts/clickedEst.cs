76d636274eb673df28487fc89c80e4ff1d8353d7
CYFERIOUS <frankdrujes@gmail.com>
2021-10-21 21:37:29 -0500

    demon is summoned

--- Assets/Scripts/clickedEst.cs
+++ Assets/Scripts/clickedEst.cs
@@ -9,9 +9,10 @@ using UnityEngine.UI;
 public class clickedEst : MonoBehaviour
 {
     // Start is called before the first frame update
-    private string[] dataBoard = new string[2];
+    private string[] dataBoard;
     List<string[]> arrayList = new List<string[]>();
     string magicWord = "DAEMONOS";
+
 
     public Canvas canvas;
     public Text myText;
@@ -36,41 +37,78 @@ public class clickedEst : MonoBehaviour
     private void TaskOnClick(int buttonIndex)
     {
       
-        //Debug.Log("You have clicked the button #" + buttons[buttonIndex].name, buttons[buttonIndex]);
-        
-        GameObject ficha = buttons[buttonIndex].transform.GetChild(0).gameObject;
-       
+            //Debug.Log("You have clicked the button #" + buttons[buttonIndex].name, buttons[buttonIndex]);
+            GameObject ficha = buttons[buttonIndex].transform.GetChild(0).gameObject;
             myText.text = ficha.name;
             string letter = ficha.name.Substring(0, ficha.name.Length/2);
-            string zona = ficha.name.Substring(1, ficha.name.Length / 2);
+            string zona = ficha.name.Substring(1, ficha.name.Length/2);
             //Debug.Log("letra" + letter);
+            dataBoard = new string[2];
             letterBoard(letter);
             //Debug.Log("zona" + zona);
             zoneBoard(zona);
             ficha.SetActive(true);
             populateList(dataBoard);
-      
+            revealDemon();
+
     }
 
     
     public void zoneBoard(string zone)
     {
         dataBoard[0] = zone;
-        Debug.Log("zona array: " + dataBoard[0].ToString());
+        //Debug.Log("zona array: " + dataBoard[0].ToString());
     }
     public void letterBoard(string letter)
     {
         dataBoard[1] = letter;
-        Debug.Log("letra array: " + dataBoard[1].ToString());
+        //Debug.Log("letra array: " + dataBoard[1].ToString());
+        
     }
     public void populateList(string[] element)
     {
         arrayList.Add(element);
-        
+        Debug.Log(arrayList);
+
         foreach (object o in arrayList)
         {
             var list = (object[])o;
-            Debug.Log("en la lista" + list[0]);
+            Debug.Log("en la lista zona" + list[0]);
+            Debug.Log("en la lista letra" + list[1]);
+           
         }
+
+    }
+    public void revealDemon()
+    {
+        string demon;
+        string s = "";
+        foreach (object o in arrayList)
+        {
+
+            var list = (object[])o;
+            string helper = Convert.ToChar(list[1]).ToString();
+            demon = helper;
+           
+            s = s + demon.ToString();
+            Debug.Log(demon);
+            /*foreach (object e in demon)
+            {
+                Debug.Log(e);
+            }*/
+
+        }
+        if (s.Length == magicWord.Length)
+        {
+            if (String.Equals(magicWord,s))
+            {
+
+                Debug.Log("MONSTRO");
+                Transform firstChild = GameObject.Find("oraqaro").transform.GetChild(0);
+                firstChild.gameObject.SetActive(true);
+            }
+        }
+        
+
     }
 }
